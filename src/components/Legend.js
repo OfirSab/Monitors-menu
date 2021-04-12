831369d28813eb4d42c34964bdd9d66ea167cb8d
Ofir saban <sabanofir1@gmail.com>
2021-04-12 10:22:10 +0300

    adding tags to the legend component

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,25 +1,40 @@
-import React , {Fragment} from 'react'
+import React, { Component , Fragment } from "react";
 import { NavItems } from '../data/Data'
 
-const Legend = (legendId) => {
-    const li = []
-    return (
-      <div className="container">
-          <ul>
-            {NavItems.Legends.map((legend,index)=>{
-                if(legend.Id == legendId.legendId){
-                    legend.tags.map((tag,index)=>{
-                        console.log(tag.Label);
-                        li.push(`<li key=${index}>${tag.Label}</li>`);
-                    })
-                    
-                }
-                })}
-                {/* {li.map(row =>{ return row})} */}
-            </ul>
-      </div>
-    )
-  }
+class Legend extends Component {
+    state = {
+        tags: []
+    } 
 
+    render(){
+        NavItems.Legends.map((legend)=>{
+            if(legend.Id == this.props.legendId){
+                legend.tags.map((tag,index)=>{
+                    this.state.tags.push(tag.Label)
+                    console.log(tag.Label);
+                    <li key={index}>{tag.Label}</li>;
+                })
+                
+            }
+            })
+            console.log("tags:"+this.state.tags);
+        console.log(this.props.legendId);
+        return (
+          <div className="container">
+                <ul className="list-group">
+              {this.state.tags.map((tag,index) => (
+                <li key={index} className="list-group-item">
+                    <div className="tag">
+                        <div className="tag-color"/>
+                        <p>{tag}</p>
+                    </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )
+      }
+    
+    }
 
 export default Legend
